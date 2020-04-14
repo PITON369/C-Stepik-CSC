dd0727ab95e39e2a18afd16297bfa48822b6a9bd
Aleksandr <PITON369@yandex.ru>
2020-04-14 21:14:10 +0300

    Update strlen.cpp

--- Step_02/strlen.cpp
+++ Step_02/strlen.cpp
@@ -5,10 +5,25 @@
 Требования к реализации: при выполнении этого задания вы можете создавать любые вспомогательные функции. Вводить или выводить что-либо не нужно. Реализовывать функцию main не нужно.
 */
 
+#include <iostream>
+using namespace std;
+
 unsigned strlen(const char *str)
 {
     int i = 0;
     for (i; str[i]; i++)
     {}
     return i;
+}
+
+void strcat(char *to, const char *from)
+{
+    int size1 = strlen (to);
+    int size2 = strlen (from);
+    
+    for (int j = size1, i = 0; i <= size2+1; j++, i++)
+    {
+
+to[j] = from[i];
+    }  
 }
